464d98bf120fba61498c87303d03df0c0d7314a6
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-07-09 17:59:36 +0900

    setup epel for centos

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,6 +27,13 @@ end
 
 case node.platform
 when 'centos'
+  # add the EPEL repo
+  yum_repository 'epel' do
+    description 'Extra Packages for Enterprise Linux'
+    mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+    gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+    action :create
+  end
   directory node.gengine.config do
     owner 'root'
     group 'root'
